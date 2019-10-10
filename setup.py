c2bd92344a5a4dba8a2d2d5b55d7da19aff4f48c
Walunt Geek <wg@walnutgeek.com>
2019-10-09 18:55:44 -0700

    move mypy run into pytest

--- setup.py
+++ setup.py
@@ -75,6 +75,7 @@ setup(
     license="Apache 2.0",
     packages=find_packages(exclude=("*.tests",)),
     package_data={"": ["file_types.json"]},
+    cmdclass=cmdclass_dict,
     entry_points={"console_scripts": ["hk=hashkernel.cli:main"]},
     install_requires=install_requires,
     extras_require={"dev": dev_requires},
