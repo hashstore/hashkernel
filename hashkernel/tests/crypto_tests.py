ce9e3a78ff9e133f6f1c31b00b2817326f8e3350
Walunt Geek <wg@walnutgeek.com>
2020-05-24 19:50:43 -0700

    add async crypto test

--- hashkernel/tests/crypto_tests.py
+++ hashkernel/tests/crypto_tests.py
@@ -9,3 +9,46 @@ def test_symetric():
     f2 = Fernet(key)
     decrypted = f2.decrypt(ciphertext)
     assert decrypted == plaintext
+
+
+from hashlib import sha256
+from cryptography.exceptions import InvalidSignature
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.asymmetric import (padding, rsa)
+
+
+def test_asymetric():
+    right_key = rsa.generate_private_key(
+        public_exponent=65537,
+        key_size=2048,
+        backend=default_backend())
+
+    wrong_key = rsa.generate_private_key(
+        public_exponent=65537,
+        key_size=2048,
+        backend=default_backend())
+
+    message = b"A message I want to sign"
+
+    right_signature = right_key.sign(message, sha256_padding(), hashes.SHA256())
+
+    wrong_signature = wrong_key.sign(message, sha256_padding(), hashes.SHA256())
+
+    right_pkey = right_key.public_key()
+
+    right_pkey.verify( right_signature, message, sha256_padding(), hashes.SHA256())
+
+    try:
+        right_pkey.verify( wrong_signature, message, sha256_padding(), hashes.SHA256())
+        assert False
+    except InvalidSignature:
+        pass
+
+
+
+def sha256_padding():
+    return padding.PSS(mgf=padding.MGF1(hashes.SHA256()),
+                       salt_length=padding.PSS.MAX_LENGTH)
+
+
